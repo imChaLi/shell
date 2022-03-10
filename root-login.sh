71a411fbb172d8dd2cdec51dd0b1ff55887df430
ichali <15619803+ichali@users.noreply.github.com>
2022-03-10 07:51:12 -0500

    2022-3-10 07:51:10

--- root-login.sh
+++ root-login.sh
@@ -1,5 +1,6 @@
-#bash <(curl -Ls https://raw.githubusercontent.com/ichali/shell/main/root-login.sh)
-echo root:ckw5282977?CKWW |sudo chpasswd root
-sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo reboot
+sudo su
+cd
+sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+echo root:ckw5282977?CKWW | sudo chpasswd
+sudo service ssh restart

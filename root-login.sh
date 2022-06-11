398025a935ee8c3bf47051937e9ff4630519c4d8
Charlie <15619803+ichali@users.noreply.github.com>
2022-06-11 11:07:33 -0400

    Delete root-login.sh

--- root-login.sh
+++
@@ -1,6 +0,0 @@
-sudo su
-cd
-sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-echo root:ckw90-=op[]chali | sudo chpasswd
-sudo service ssh restart

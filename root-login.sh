5e4405487f8e82280e1ba7a8daae59cea521c0c0
Charlie <15619803+ichali@users.noreply.github.com>
2022-06-11 11:06:53 -0400

    Update root-login.sh

--- root-login.sh
+++ root-login.sh
@@ -2,5 +2,5 @@ sudo su
 cd
 sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-echo root:ckw5282977?CKWW | sudo chpasswd
+echo root:ckw90-=op[]chali | sudo chpasswd
 sudo service ssh restart

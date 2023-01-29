550fb56a21dce7607d8db07f81c4840359b879ac
Charlie <15619803+ichali@users.noreply.github.com>
2023-01-29 21:37:59 +0000

    Create root_pass.sh

---
+++ root_pass.sh
@@ -0,0 +1,6 @@
+sudo su
+cd
+sudo sed -i 's/^.*PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sudo sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+echo root:$$CHANGE_PASS$$ | sudo chpasswd
+sudo service ssh restart
